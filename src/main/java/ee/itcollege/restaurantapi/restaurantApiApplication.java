e51d83e942a15f89d64de33f599618fac3ede71d
jPluutus <JyrnasPluutus@gmail.com>
2019-05-20 21:14:42 +0300

    Initial commit

---
+++ src/main/java/ee/itcollege/restaurantapi/restaurantApiApplication.java
@@ -0,0 +1,13 @@
+package ee.itcollege.restaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class restaurantApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(restaurantApiApplication.class, args);
+    }
+
+}
